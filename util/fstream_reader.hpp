49865d738040b52d2b6c62ca9ac7587c8d995ac1
intns <84647527+intns@users.noreply.github.com>
2021-09-20 19:09:18 +0100

    Fix misc. signed/unsigned issues

--- util/fstream_reader.hpp
+++ util/fstream_reader.hpp
@@ -48,26 +48,16 @@ public:
     }
     inline u16 readU16()
     {
-        u8 byte0 = readU8();
-        u8 byte1 = readU8();
-
-        return (m_endianness == Endianness::Little) ? (byte0 | (byte1 << 8)) : (byte1 | (byte0 << 8));
+        u16 v = 0;
+        read(reinterpret_cast<char*>(&v), 2);
+        return ((v & 0xff00) >> 8) | ((v & 0x00ff) << 8);
     }
     inline u32 readU32()
     {
         u32 v = 0;
         read(reinterpret_cast<char*>(&v), 4);
-
         return (((v & 0xff000000) >> 24) | ((v & 0x00ff0000) >> 8) | ((v & 0x0000ff00) << 8)
                 | ((v & 0x000000ff) << 24));
-    }
-    inline u64 readU64()
-    {
-        u32 word0 = readU32();
-        u32 word1 = readU32();
-
-        return (m_endianness == Endianness::Little) ? (((u64)word0) | ((u64)word1 << 32))
-                                                    : (((u64)word1) | ((u64)word0 << 32));
     }
 
     inline s8 readS8()
@@ -78,27 +68,16 @@ public:
     }
     inline s16 readS16()
     {
-        s8 byte0 = readS8();
-        s8 byte1 = readS8();
-
-        return (m_endianness == Endianness::Little) ? (byte0 | (byte1 << 8)) : (byte1 | (byte0 << 8));
+        s16 v = 0;
+        read(reinterpret_cast<char*>(&v), 2);
+        return ((v & 0xff00) >> 8) | ((v & 0x00ff) << 8);
     }
     inline s32 readS32()
     {
-        s8 byte0 = readS8();
-        s8 byte1 = readS8();
-        s8 byte2 = readS8();
-        s8 byte3 = readS8();
-
-        return (m_endianness == Endianness::Little) ? (byte0 | (byte1 << 8) | (byte2 << 16) | (byte3 << 24))
-                                                    : (byte3 | (byte2 << 8) | (byte1 << 16) | (byte0 << 24));
-    }
-    inline s64 readS64()
-    {
-        s32 word0 = readS32();
-        s32 word1 = readS32();
-
-        return (m_endianness == Endianness::Little) ? (((s64)word0 << 32) | (word1)) : (((s64)word1 << 32) | (word0));
+        s32 v = 0;
+        read(reinterpret_cast<char*>(&v), 4);
+        return (((v & 0xff000000) >> 24) | ((v & 0x00ff0000) >> 8) | ((v & 0x0000ff00) << 8)
+                | ((v & 0x000000ff) << 24));
     }
 
     inline f32 readF32()
