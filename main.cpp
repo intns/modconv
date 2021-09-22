b7712c787cfa588954ee4ec5670450b26144c83f
intns <84647527+intns@users.noreply.github.com>
2021-09-22 08:44:50 +0100

    Fix export_materials

--- main.cpp
+++ main.cpp
@@ -26,7 +26,7 @@ static std::vector<Command> gCommands = {
 
     { "NEW_LINE" },
 
-    { "export_materials ", { "output filename" }, " exports all materials to a file ", cmd::mod::exportMaterials },
+    { "export_materials", { "output filename" }, " exports all materials to a file ", cmd::mod::exportMaterials },
     { "export_textures", { "output directory"}, "exports all textures to a directory", cmd::mod::exportTextures },
     { "export_ini", { "output filename" }, "exports the ini to a file", cmd::mod::exportMaterials },
     { "export_obj", { "output filename" }, "exports the model to an obj file", cmd::mod::exportObj },
