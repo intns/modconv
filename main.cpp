ab5ae8bffbdaee439928118959353461f80b253d
intns <84647527+intns@users.noreply.github.com>
2021-09-22 16:23:35 +0100

    Fix export ini

--- main.cpp
+++ main.cpp
@@ -28,7 +28,7 @@ static std::vector<Command> gCommands = {
 
     { "export_materials", { "output filename" }, " exports all materials to a file ", cmd::mod::exportMaterials },
     { "export_textures", { "output directory"}, "exports all textures to a directory", cmd::mod::exportTextures },
-    { "export_ini", { "output filename" }, "exports the ini to a file", cmd::mod::exportMaterials },
+    { "export_ini", { "output filename" }, "exports the ini to a file", cmd::mod::exportIni },
     { "export_obj", { "output filename" }, "exports the model to an obj file", cmd::mod::exportObj },
 
     { "NEW_LINE" },
